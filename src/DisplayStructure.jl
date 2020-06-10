08b1547bfc71ec947fa2e45503b4ec99b2e535cf
JingYu Ning <foldfelis@gmail.com>
2020-06-10 18:04:40 +0800

    refactor

--- src/DisplayStructure.jl
+++ src/DisplayStructure.jl
@@ -1,5 +1,6 @@
 module DisplayStructure
 
+    include("display_row.jl")
     include("display_array.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 
 @testset "DisplayStructure.jl" begin
 
+    include("display_row.jl")
     include("display_array.jl")
 
 end
